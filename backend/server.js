be65f9bfd2d4d5f4772bffcf485beac75534eaa8
Tobe Marizu <tobemarizu@gmail.com>
2026-08-03 15:40:51 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173", // local dev
+    "https://phishinguard-three.vercel.app" // production
   ],
   credentials: true,
 }));
